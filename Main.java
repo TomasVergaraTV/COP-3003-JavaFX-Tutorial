bbd88284e9f859e0ec9381708628b53e594a3849
TomasVergaraTV <32168275+TomasVergaraTV@users.noreply.github.com>
2018-09-11 11:31:09 -0400

    JavaFX 2 Main
    Added Main.java from JavaFX 2 to the JavaFX 2 Branch

--- Main.java
+++ Main.java
@@ -3,9 +3,20 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,20 +27,51 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    primaryStage.setTitle("Hello World!");
-    Button btn = new Button();
-    btn.setText("Say 'Hello World'");
+    primaryStage.setTitle("JavaFX Welcome"); // Sets title on top of window
+
+    GridPane grid = new GridPane(); // Initializes new grid used to organize elements in window
+    grid.setAlignment(Pos.CENTER); // Default position of grid set to center instead of top left
+    grid.setHgap(10); // Spacing between rows and columns
+    grid.setVgap(10);
+    grid.setPadding(new Insets(25, 25, 25, 25)); // padding on INSIDE of window
+
+    Text scenetitle = new Text("Welcome"); // Sets title, cannot be edited
+    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20)); // Changes font
+    grid.add(scenetitle, 0, 0, 2, 1); // add to grid and location
+
+    Label userName = new Label("User Name:"); // Adds label to textbox used for username
+    grid.add(userName, 0, 1); // adds to grid and location
+
+    TextField userTextField = new TextField(); // same but with textfield
+    grid.add(userTextField, 1, 1);
+
+    Label pw = new Label("Password:");
+    grid.add(pw, 0, 2);
+
+    PasswordField pwBox = new PasswordField(); // creates password field (different from normal textbox)
+    grid.add(pwBox, 1, 2); // adds to gird and location
+
+    Button btn = new Button("Sign in");
+    HBox hbBtn = new HBox(10);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT); // sets hbBtn to bottom right position
+    hbBtn.getChildren().add(btn); // sets btn to hbBtn
+    grid.add(hbBtn, 1, 4); // adds to grid and location
+
+    final Text actiontarget = new Text();
+    grid.add(actiontarget, 1, 6);
+
     btn.setOnAction(new EventHandler<ActionEvent>() {
 
       @Override
-      public void handle(ActionEvent event) {
-        System.out.println("Hello World!");
+      public void handle(ActionEvent e) { // creates event that happens when button is pressed
+        actiontarget.setFill(Color.FIREBRICK);
+        actiontarget.setText("Sign in button pressed");
       }
     });
 
-    StackPane root = new StackPane();
-    root.getChildren().add(btn);
-    primaryStage.setScene(new Scene(root, 300, 250));
+    Scene scene = new Scene(grid, 300, 275); // sets size of window
+    primaryStage.setScene(scene);
+
     primaryStage.show();
   }
 }
