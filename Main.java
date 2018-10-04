61071166ba2f451c7724712426b7236f72a9be44
TomasVergaraTV <32168275+TomasVergaraTV@users.noreply.github.com>
2018-10-03 22:59:50 -0400

    Java Tutorial 4 Upload
    Implemented fxml, and javascript

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 package sample;
 
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,21 +15,13 @@ public class Main extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {
-    primaryStage.setTitle("Hello World!");
-    Button btn = new Button();
-    btn.setText("Say 'Hello World'");
-    btn.setOnAction(new EventHandler<ActionEvent>() {
+  public void start(Stage stage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-      @Override
-      public void handle(ActionEvent event) {
-        System.out.println("Hello World!");
-      }
-    });
+    Scene scene = new Scene(root, 300, 275);
 
-    StackPane root = new StackPane();
-    root.getChildren().add(btn);
-    primaryStage.setScene(new Scene(root, 300, 250));
-    primaryStage.show();
+    stage.setTitle("FXML Welcome");
+    stage.setScene(scene);
+    stage.show();
   }
 }
